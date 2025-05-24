0b2f15838f051425d647e5146f6889219161bb86
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-24 15:51:41 +0530

    update

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,7 +1,13 @@
 export default function Product(props) {
-  return <div>
-    <h2>{props.value.name}</h2>
-    <h3>{props.value.price}</h3>
-    <button>Add to Cart</button>
-  </div>;
+  if (!props.value) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{props.value.name}</h2>
+      <h3>{props.value.price}</h3>
+      <button>Add to Cart</button>
+    </div>
+  );
 }
